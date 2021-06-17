a4684b0feccef71e369f1b9afae787cb71bf6575
baynejiang <baynejiang@tencent.com>
2021-06-17 11:05:54 +0800

    逆序对问题

--- src/class04/Code03_ReversePair.go
+++ src/class04/Code03_ReversePair.go
@@ -1,5 +1,9 @@
 package class04
 
+// ReversePair 在一个数组中，
+// 任何一个前面的数a，和任何一个后面的数b，
+// 如果(a,b)是降序的，就称为逆序对
+// 返回数组中所有的逆序对
 func ReversePair(arr []int) int {
 	return process3(arr, 0, len(arr)-1)
 }
