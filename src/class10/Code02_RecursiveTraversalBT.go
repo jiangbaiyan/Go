434b4ae0b44ab1c7e6ac2f05a29f4cd9a9c73f07
baynejiang <baynejiang@tencent.com>
2021-07-02 12:44:45 +0800

    树递归遍历

--- src/class10/Code02_RecursiveTraversalBT.go
+++ src/class10/Code02_RecursiveTraversalBT.go
@@ -1,7 +1,39 @@
 package class10
 
+import "fmt"
+
 type TreeNode struct {
 	left  *TreeNode
 	right *TreeNode
 	value interface{}
+}
+
+// Pre 先序
+func Pre(head *TreeNode) {
+	if head == nil {
+		return
+	}
+	fmt.Println(head.value)
+	Pre(head.left)
+	Pre(head.right)
+}
+
+// In 中序
+func In(head *TreeNode) {
+	if head == nil {
+		return
+	}
+	In(head.left)
+	fmt.Println(head.value)
+	In(head.right)
+}
+
+// Pos 后序
+func Pos(head *TreeNode) {
+	if head == nil {
+		return
+	}
+	Pos(head.left)
+	Pos(head.right)
+	fmt.Println(head.value)
 }
