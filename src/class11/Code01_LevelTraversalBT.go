52fc09ae76948549626be16cb9c8c86ca6b5ac41
baynejiang <baynejiang@tencent.com>
2021-07-05 09:48:35 +0800

    树层序遍历

---
+++ src/class11/Code01_LevelTraversalBT.go
@@ -0,0 +1,31 @@
+package class11
+
+import (
+	"fmt"
+	"github.com/jiangbaiyan/go/src/class03"
+)
+
+type Node struct {
+	value interface{}
+	left  *Node
+	right *Node
+}
+
+// LevelTraversalBT 层序遍历
+func LevelTraversalBT(head *Node) {
+	if head == nil {
+		return
+	}
+	queue := &class03.MyRingQueue{}
+	queue.Push(head)
+	for !queue.IsEmpty() {
+		cur := queue.Pop().(*Node)
+		fmt.Println(cur.value)
+		if cur.left != nil {
+			queue.Push(cur.left)
+		}
+		if cur.right != nil {
+			queue.Push(cur.right)
+		}
+	}
+}
